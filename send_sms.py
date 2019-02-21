031f54b9a7f2af9b5fc88b996f06dbceb7cf5ae6
arfath19 <37550475+arfath19@users.noreply.github.com>
2019-02-21 12:48:48 +0530

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACb30fb494cc752aae4cdb99fac4d31b5a'
+auth_token = 'db0302fa2ac0e022f763d6dbbf9eeb48'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+19292078819',
+                     to='+918978434569'
+                 )
+
+print(message.sid)
